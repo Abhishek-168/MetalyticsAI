e42e4072606ad5e2bdd790190e17e684de271be7
Abhishek Jain <172581933+Abhishek-168@users.noreply.github.com>
2025-09-17 16:36:00 +0530

    deplyment done

--- LCA_AI/Frontend/src/Components/chatbot.tsx
+++ LCA_AI/Frontend/src/Components/chatbot.tsx
@@ -20,7 +20,7 @@ const Chatbot: React.FC = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/chat", {
+      const res = await fetch("https://metalyticsai-chatbot.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage }),
